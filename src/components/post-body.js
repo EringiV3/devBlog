cc8700bcbfe3dba3580cf5337815a99f04e5bd7b
Kodai <39426616+EringiV3@users.noreply.github.com>
2020-09-13 11:31:32 +0900

    fix post-body

--- src/components/post-body.js
+++ src/components/post-body.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import ReactHtmlParser from "react-html-parser"
 import Prism from "prismjs"
+import "prismjs/components/prism-jsx.min"
 import "prismjs/themes/prism.css"
 import Imgix from "react-imgix"
 import unified from "unified"
@@ -32,21 +33,23 @@ export default ({ loopContents }) => {
   useEffect(() => {
     Prism.highlightAll()
   }, [])
+
+  const RenderAst = ({ target }) => {
+    const htmlAst = unified().use(parse, { fragment: true }).parse(target)
+    return renderAst(htmlAst)
+  }
+  const HtmlParser = ({ htmlString }) => ReactHtmlParser(htmlString)
+
   return (
     <>
       <div className="postbody">
-        {loopContents.map((content, i) => {
-          if (content.fieldId === LOOP_CONTENT_FIELD_ID_RICH_EDITOR) {
-            const htmlAst = unified()
-              .use(parse, { fragment: true })
-              .parse(content.richEditor)
-            return <div key={i}>{renderAst(htmlAst)}</div>
-          } else if (content.fieldId === LOOP_CONTENT_FIELD_ID_HTML) {
-            return <div key={i}>{ReactHtmlParser(content.html)}</div>
-          } else {
-            return null
-          }
-        })}
+        {loopContents.map((content, i) =>
+          content.fieldId === LOOP_CONTENT_FIELD_ID_RICH_EDITOR ? (
+            <RenderAst key={i} target={content.richEditor} />
+          ) : content.fieldId === LOOP_CONTENT_FIELD_ID_HTML ? (
+            <HtmlParser key={i} htmlString={content.html} />
+          ) : null
+        )}
       </div>
       <style jsx>{`
         .postbody {
